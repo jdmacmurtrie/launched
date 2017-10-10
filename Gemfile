80f4e4841f50c7b97904bb09ea328e759db06639
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-10-10 11:01:46 -0400

    add react

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org/'
 
+ruby "2.3.3"
+
 gem 'rails', '~> 5.1.0'
 gem 'pg', '~> 0.18'
 gem 'puma', '~> 3.0'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is a test of a test' do
+    expect(true).to be true
+  end
 end
